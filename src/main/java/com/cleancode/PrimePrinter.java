a92e8b5698fa1e433b30f6c11e135892b1117ca1
Duckki Lee <csleedk@gmail.com>
2022-11-28 13:11:05 +0900

    리팩토링
    소수를 찾는 부분과 소수를 출력하는 부분을 각각 findPrimeNumbers(), printPrimeNumbers() 로 추출
    local 변수의 첫 사용 위치로 선언 이동 : n, possiblyPrime, rowoffset, column

--- src/main/java/com/cleancode/PrimePrinter.java
+++ src/main/java/com/cleancode/PrimePrinter.java
@@ -3,29 +3,36 @@ package com.cleancode;
 public class PrimePrinter {
     public static void main(String[] args) {
         final int numberOfPrimes = 1000;
-        final int linesPerPage = 50;
-        final int columns = 4;
-        final int ORDMAX = 30;
+        int primes[];
+
+        primes = findPrimeNumbers(numberOfPrimes);
+
+        printPrimeNumbers(numberOfPrimes, primes);
+    }
+
+    private static int[] findPrimeNumbers(int numberOfPrimes) {
         int primes[] = new int[numberOfPrimes+1];
-        int pagenumber;
-        int pageoffset;
-        int rowoffset;
-        int column;
-        int candidate;
-        int primeIndex;
-        boolean possiblyPrime;
-        int ord;
-        int square;
-        int n=0;
+        primes[1] = 2;
+
+        final int ORDMAX = 30;
         int multiples[] = new int[ORDMAX+1];
 
+
+        int candidate;
         candidate=1;
+
+        int primeIndex;
         primeIndex=1;
-        primes[1] = 2;
+
+        int ord;
         ord = 2;
+
+        int square;
         square = 9;
 
+
         while (primeIndex < numberOfPrimes) {
+            boolean possiblyPrime;
             do {
                 candidate += 2;
                 if( candidate == square) {
@@ -33,7 +40,8 @@ public class PrimePrinter {
                     square=primes[ord]*primes[ord];
                     multiples[ord-1]=candidate;
                 }
-                n=2;
+
+                int n=2;
                 possiblyPrime=true;
                 while (n < ord && possiblyPrime) {
                     while (multiples[n]<candidate)
@@ -46,16 +54,27 @@ public class PrimePrinter {
             primeIndex++;
             primes[primeIndex]=candidate;
         }
+        return primes;
+    }
+
+    private static void printPrimeNumbers(int numberOfPrimes, int[] primes) {
+        final int linesPerPage = 50;
+        final int columns = 4;
+
+        int pagenumber;
+        int pageoffset;
         pagenumber = 1;
         pageoffset = 1;
+
         while (pageoffset <= numberOfPrimes) {
             System.out.print("The First ");
             System.out.print(numberOfPrimes);
             System.out.print(" Prime Numbers === Page ");
             System.out.print(pagenumber);
             System.out.println("\n");
-            for (rowoffset=pageoffset; rowoffset <= pageoffset+linesPerPage-1; rowoffset++) {
-                for (column = 0; column <= columns - 1; column++)
+
+            for (int rowoffset=pageoffset; rowoffset <= pageoffset+linesPerPage-1; rowoffset++) {
+                for (int column = 0; column <= columns - 1; column++)
                     if (rowoffset + column * linesPerPage <= numberOfPrimes)
                         System.out.printf("%10d", primes[rowoffset + column * linesPerPage]);
                 System.out.println();
