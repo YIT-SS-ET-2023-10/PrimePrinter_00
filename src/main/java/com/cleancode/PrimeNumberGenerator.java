8b25e52ad9213d89bf7e8a04a5811f82afadc297
Duckki Lee <csleedk@gmail.com>
2022-11-28 13:29:35 +0900

    리팩토링
    findPrimeNumbers() 메소드를 PrimeNumberGenerator 클래스로 전환

---
+++ src/main/java/com/cleancode/PrimeNumberGenerator.java
@@ -0,0 +1,56 @@
+package com.cleancode;
+
+public class PrimeNumberGenerator {
+    int primes[];
+    int numberOfPrimes;
+    final int ORDMAX = 30;
+    int multiples[];
+
+    public PrimeNumberGenerator(int numberOfPrimes) {
+        this.numberOfPrimes = numberOfPrimes;
+        this.primes = new int[numberOfPrimes + 1];
+        primes[1] = 2;
+    }
+
+    public int[] findPrimeNumbers() {
+        multiples = new int[ORDMAX + 1];
+
+        int candidate;
+        candidate = 1;
+
+        int primeIndex;
+        primeIndex = 1;
+
+        int ord;
+        ord = 2;
+
+        int square;
+        square = 9;
+
+
+        while (primeIndex < numberOfPrimes) {
+            boolean possiblyPrime;
+            do {
+                candidate += 2;
+                if (candidate == square) {
+                    ord++;
+                    square = primes[ord] * primes[ord];
+                    multiples[ord - 1] = candidate;
+                }
+
+                int n = 2;
+                possiblyPrime = true;
+                while (n < ord && possiblyPrime) {
+                    while (multiples[n] < candidate)
+                        multiples[n] += primes[n] + primes[n];
+                    if (multiples[n] == candidate)
+                        possiblyPrime = false;
+                    n++;
+                }
+            } while (!possiblyPrime);
+            primeIndex++;
+            primes[primeIndex] = candidate;
+        }
+        return primes;
+    }
+}
