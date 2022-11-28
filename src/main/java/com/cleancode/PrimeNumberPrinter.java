b8119ce5a52fc57a22f7f02045fa2ab6d503d081
Duckki Lee <csleedk@gmail.com>
2022-11-28 13:17:16 +0900

    리팩토링
    printPrimeNumbers() , printPageHead() 와 두 메소드를 PrimeNumberPrinter 클래스로 전환

---
+++ src/main/java/com/cleancode/PrimeNumberPrinter.java
@@ -0,0 +1,44 @@
+package com.cleancode;
+
+public class PrimeNumberPrinter {
+    final int linesPerPage = 50;
+    final int columns = 4;
+    int pagenumber;
+    int pageoffset;
+    int numberOfPrimes;
+    int[] primes;
+
+    public PrimeNumberPrinter(int[] primes) {
+        this.primes = primes;
+        numberOfPrimes = primes.length-1;
+    }
+
+    public void printPrimeNumbers() {
+        pagenumber = 1;
+        pageoffset = 1;
+
+        while (pageoffset <= numberOfPrimes) {
+            printPageHead(numberOfPrimes, pagenumber);
+
+            for (int rowoffset=pageoffset; rowoffset <= pageoffset+linesPerPage-1; rowoffset++) {
+                for (int column = 0; column <= columns - 1; column++)
+                    if (rowoffset + column * linesPerPage <= numberOfPrimes)
+                        System.out.printf("%10d", primes[rowoffset + column * linesPerPage]);
+                System.out.println();
+            }
+            System.out.println("\f");
+
+            pagenumber++;
+            pageoffset += linesPerPage*columns;
+
+        }
+    }
+
+    public static void printPageHead(int numberOfPrimes, int pagenumber) {
+        System.out.print("The First ");
+        System.out.print(numberOfPrimes);
+        System.out.print(" Prime Numbers === Page ");
+        System.out.print(pagenumber);
+        System.out.println("\n");
+    }
+}
